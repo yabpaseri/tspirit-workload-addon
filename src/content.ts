d04aeb9144fd94bf90acb1b96fac523f2d12ed48
yabpaseri <yab.82.hiro@gmail.com>
2023-08-20 14:53:39 +0900

    inject-managerを消して、contentsに寄せた

--- src/content.ts
+++ src/content.ts
@@ -1,5 +1,8 @@
-import { InjectManager } from './inject-manager';
+import { ALL_ADDONS } from './addons';
+import { Addon, JobAssignAddon, WorkBalanceAddon } from './addons/addon-base';
 import { Preference } from './preference';
+
+// content-script用
 
 declare const window: Window['window'] & {
 	tsw: {
@@ -11,17 +14,34 @@ declare const window: Window['window'] & {
 	window.tsw = window.tsw ?? {};
 	window.tsw.preference = Preference;
 
-	const manager = new InjectManager();
+	// DOM要素管理
+	const elements = new (class {
+		public empWorkDialog: HTMLElement | undefined;
+		public empJobAssign: HTMLElement | undefined;
+		get isComplete() {
+			return this.empJobAssign != null && this.empJobAssign != null;
+		}
+	})();
+
+	// Addon#inject実行
+	const inject = (clazz: typeof Addon, ele: HTMLElement) => {
+		for (const addon of ALL_ADDONS) {
+			if (addon instanceof clazz) addon.inject(ele);
+		}
+	};
+
 	function mutationCallback(mutations: MutationRecord[], observer: MutationObserver) {
 		// 追加された要素の中から「empWorkDialog」または「empJobAssign」のIDを持つ要素を探す
 		for (const node of mutations.flatMap((r) => Array.from(r.addedNodes))) {
 			if (!(node instanceof HTMLElement)) continue;
 			if (node.id === 'empWorkDialog') {
-				manager.setEmpWorkDialog(node);
+				elements.empWorkDialog = node;
+				inject(WorkBalanceAddon, node);
 			} else if (node.id === 'empJobAssign') {
-				manager.setEmpJobAssign(node);
+				elements.empJobAssign = node;
+				inject(JobAssignAddon, node);
 			}
-			if (manager.iscomplete) {
+			if (elements.isComplete) {
 				observer.disconnect();
 				break;
 			}
